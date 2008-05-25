1720c3b23dc805b9af4862a09b0d2f17859b9999
Martin Mares <mj@ucw.cz>
2008-05-25 22:51:56 +0200

    Move more things to mop.cf and also include userlist.
    The old method of listing users by matching UID's is no longer supported.

--- mop/eval/mo-get-users.sh
+++ mop/eval/mo-get-users.sh
@@ -3,21 +3,12 @@
 if [ "$1" = --help ] ; then
 	echo "Usage: mo-get-users [--full]"
 fi
-[ -f config ] || { echo "Missing config file, check cwd." ; exit 1 ; }
+[ -f cf/mop ] || { echo "Missing config file, check cwd." ; exit 1 ; }
 set -e
-. config
+. cf/mop
 
-if [ -z "$CT_USER_LIST" ] ; then
-	if [ "$1" = --full ] ; then
-		FORM='$1,$5'
-	else
-		FORM='$1'
-	fi
-	awk -F: </etc/passwd "{ gsub(\",.*\",\"\",\$5); OFS=\"\t\"; if (\$3 >= $CT_UID_MIN && \$3 <= $CT_UID_MAX) print $FORM; }"
+if [ "$1" = --full ] ; then
+	cut -d '	' -f 1,2 <$CT_USER_LIST
 else
-	if [ "$1" = --full ] ; then
-		cut -d '	' -f 1,2 <$CT_USER_LIST
-	else
-		cut -d '	' -f 1 <$CT_USER_LIST
-	fi
+	cut -d '	' -f 1 <$CT_USER_LIST
 fi
