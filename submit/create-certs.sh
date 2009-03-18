ca3d30ad48d1db68ab47854d47a660914bead553
Martin Mares <mj@ucw.cz>
2009-03-18 14:16:25 +0100

    Updated create-certs to the new directory layout.

--- submit/create-certs.sh
+++ submit/create-certs.sh
@@ -3,7 +3,7 @@
 # (c) 2007 Martin Mares <mj@ucw.cz>
 
 set -e
-if [ ! -f submit/ca-cert.tpl ] ; then
+if [ ! -f lib/ca-cert.tpl ] ; then
 	echo >&2 "Please run from the MO root directory."
 	exit 1
 fi
@@ -13,20 +13,20 @@ rm -rf certs
 mkdir certs
 
 echo "### Creating CA certificate ###"
-submit/privkey >certs/ca-key.pem
-certtool --generate-self-signed --load-privkey certs/ca-key.pem --outfile certs/ca-cert.pem --template submit/ca-cert.tpl
+bin/privkey >certs/ca-key.pem
+certtool --generate-self-signed --load-privkey certs/ca-key.pem --outfile certs/ca-cert.pem --template lib/ca-cert.tpl
 
 echo "### Creating server certificate ###"
-submit/privkey >certs/server-key.pem
-certtool --generate-request --load-privkey certs/server-key.pem --outfile certs/server-req.pem --template submit/server-cert.tpl
-certtool --generate-certificate --load-request certs/server-req.pem --outfile certs/server-cert.pem --load-ca-certificate certs/ca-cert.pem --load-ca-privkey certs/ca-key.pem --template submit/server-cert.tpl
+bin/privkey >certs/server-key.pem
+certtool --generate-request --load-privkey certs/server-key.pem --outfile certs/server-req.pem --template lib/server-cert.tpl
+certtool --generate-certificate --load-request certs/server-req.pem --outfile certs/server-cert.pem --load-ca-certificate certs/ca-cert.pem --load-ca-privkey certs/ca-key.pem --template lib/server-cert.tpl
 
 seq=1
 for user in `bin/mo-get-users` ; do
 	seq=$(($seq+1))
 	echo "### Creating certificate for user #$seq ($user) ###"
-	sed <submit/client-cert.tpl >certs/$user-cert.tpl "s/cn = \".*\"/cn = \"$user\"/; s/serial = .*/serial = $seq/;"
-	submit/privkey >certs/$user-key.pem
+	sed <lib/client-cert.tpl >certs/$user-cert.tpl "s/cn = \".*\"/cn = \"$user\"/; s/serial = .*/serial = $seq/;"
+	bin/privkey >certs/$user-key.pem
         certtool --generate-request --load-privkey certs/$user-key.pem --outfile certs/$user-req.pem --template certs/$user-cert.tpl
         certtool --generate-certificate --load-request certs/$user-req.pem --outfile certs/$user-cert.pem --load-ca-certificate certs/ca-cert.pem --load-ca-privkey certs/ca-key.pem --template certs/$user-cert.tpl
 done
