e4b621dcc174bc0083bf7627f911bb9bd5d6b36e
Martin Mares <mj@ucw.cz>
2012-03-30 20:50:09 +0200

    mo-backup: Do not forget to back up submit server

--- mop/admin/mo-backup.sh
+++ mop/admin/mo-backup.sh
@@ -7,12 +7,17 @@ else
 	D=$1
 fi
 mkdir -p $D
-for m in `seq 27 74` ; do
-	m="ceoi$m"
+
+for m in 13 14 15 23 24 25 {3,4,5,6,7,8}{1,2,3,4,5} ; do
+	m="mo$m"
 	echo -n "$m: "
-	mkdir $p $D/$m
+	mkdir -p $D/$m
 	pushd $D/$m >/dev/null
 	ssh root@$m 'cd /mo/users ; tar czf - . --exclude=.kde' | tar xzf -
 	popd >/dev/null
 	du -s $D/$m | cut -f 1
 done
+
+echo -n "submit: "
+rsync -a mo-submit@mo21: $D/submit/
+du -s $D/submit | cut -f 1
